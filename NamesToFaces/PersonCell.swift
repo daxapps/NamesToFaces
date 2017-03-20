88e2380f2fdb06a4732b2cf8419c3ec26d0f0c3d
daxapps <daxapps777@gmail.com>
2017-03-20 07:37:51 -0500

    Setup UI

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Jason Crawford on 3/20/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
